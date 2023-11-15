178e0466e5fc89c2e241afc19a48c4b3a572d6be
sokainaSB <sokaiina.sbaii@gmail.com>
2023-11-15 03:10:43 +0100

    new

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicon souapptesou.wsgi:application --log-file -
--- souapptesou/settings.py
+++ souapptesou/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
+
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +128,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+
+django_heroku.settings(locals())
